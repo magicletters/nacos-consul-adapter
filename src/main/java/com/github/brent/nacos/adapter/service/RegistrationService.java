15b9649b15de520a3eac8c9dcbf0737582b8c7d8
zhujia <magicletters@qq.com>
2020-09-22 13:59:09 +0800

    增加health url

--- src/main/java/com/github/brent/nacos/adapter/service/RegistrationService.java
+++ src/main/java/com/github/brent/nacos/adapter/service/RegistrationService.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright (c) 2019 Brent
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,24 +23,17 @@
 
 package com.github.brent.nacos.adapter.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Supplier;
-
+import com.github.brent.nacos.adapter.data.ChangeItem;
+import com.github.brent.nacos.adapter.data.Service;
+import com.github.brent.nacos.adapter.data.ServiceHealth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
-
-import com.github.brent.nacos.adapter.data.ChangeItem;
-
 import rx.Single;
+
+import java.util.*;
+import java.util.function.Supplier;
 
 /**
  * Returns Services and List of Service with its last changed
@@ -48,57 +41,100 @@ import rx.Single;
 @Component
 public class RegistrationService {
 
-	private static final String[] NO_SERVICE_TAGS = new String[0];
+    private static final String[] NO_SERVICE_TAGS = new String[0];
 
-	@Autowired
-	private DiscoveryClient discoveryClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-	public Single<ChangeItem<Map<String, String[]>>> getServiceNames(long waitMillis, Long index) {
-		return returnDeferred(waitMillis, index, () -> {
-			List<String> services = discoveryClient.getServices();
-			Set<String> set = new HashSet<String>();
-			set.addAll(services);
+    public Single<ChangeItem<Map<String, String[]>>> getServiceNames(long waitMillis, Long index) {
+        return returnDeferred(waitMillis, index, () -> {
+            List<String> services = discoveryClient.getServices();
+			Set<String> set = new HashSet<>(services);
 
-			Map<String, String[]> result = new HashMap<String, String[]>();
-			for (String item : set) {
-				result.put(item, NO_SERVICE_TAGS);
-			}
-			return result;
-		});
-	}
+            Map<String, String[]> result = new HashMap<>();
+            for (String item : set) {
+                result.put(item, NO_SERVICE_TAGS);
+            }
+            return result;
+        });
+    }
 
-	public Single<ChangeItem<List<Map<String, Object>>>> getService(String appName, long waitMillis, Long index) {
-		return returnDeferred(waitMillis, index, () -> {
-			List<ServiceInstance> instances = discoveryClient.getInstances(appName);
-			List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+    public Single<ChangeItem<List<Service>>> getService(String appName, long waitMillis, Long index) {
+        return returnDeferred(waitMillis, index, () -> {
+            List<ServiceInstance> instances = discoveryClient.getInstances(appName);
+            List<Service> list = new ArrayList<>();
 
-			if (instances == null) {
-				return Collections.emptyList();
-			} else {
-				Set<ServiceInstance> instSet = new HashSet<ServiceInstance>(instances);
-				for (ServiceInstance instance : instSet) {
-					Map<String, Object> ipObj = new HashMap<String, Object>();
+            if (instances == null) {
+                return Collections.emptyList();
+            } else {
+                Set<ServiceInstance> instSet = new HashSet<>(instances);
+                for (ServiceInstance instance : instSet) {
+                    Map<String, String> metaJo = new HashMap<>();
+                    metaJo.put("management.port", "" + instance.getPort());
 
-					ipObj.put("Address", instance.getHost());
-					ipObj.put("Node", instance.getServiceId());
-					ipObj.put("ServiceAddress", instance.getHost());
-					ipObj.put("ServiceName", instance.getServiceId());
-					ipObj.put("ServiceID", instance.getHost() + ":" + instance.getPort());
-					ipObj.put("ServicePort", instance.getPort());
-					ipObj.put("NodeMeta", Collections.emptyMap());
-					Map<String, String> metaJo = new HashMap<String, String>();
-					metaJo.put("management.port", "" + instance.getPort());
-					ipObj.put("ServiceMeta", metaJo);
-					ipObj.put("ServiceTags", Collections.emptyList());
+                    Service ipObj = Service.builder()
+                                           .address(instance.getHost())
+                                           .node(instance.getServiceId())
+                                           .serviceAddress(instance.getHost())
+                                           .serviceName(instance.getServiceId())
+                                           .serviceID(instance.getHost() + ":" + instance.getPort())
+                                           .servicePort(instance.getPort())
+                                           .nodeMeta(Collections.emptyMap())
+                                           .serviceMeta(metaJo)
+                                           .serviceTags(Collections.emptyList())
+                                           .build();
 
-					list.add(ipObj);
-				}
-				return list;
-			}
-		});
-	}
 
-	private <T> Single<ChangeItem<T>> returnDeferred(long waitMillis, Long index, Supplier<T> fn) {
-		return Single.just(new ChangeItem<>(fn.get(), new Date().getTime()));
-	}
+                    list.add(ipObj);
+                }
+                return list;
+            }
+        });
+    }
+
+    public Single<ChangeItem<List<ServiceHealth>>> getServiceHealth(String appName, long waitMillis, Long index) {
+        return returnDeferred(waitMillis, index, () -> {
+            List<ServiceInstance> instances = discoveryClient.getInstances(appName);
+            List<ServiceHealth> list = new ArrayList<>();
+
+            if (instances == null) {
+                return Collections.emptyList();
+            } else {
+                Set<ServiceInstance> instSet = new HashSet<>(instances);
+                for (ServiceInstance instance : instSet) {
+                    ServiceHealth.Node node = ServiceHealth.Node.builder()
+                                                                .name(instance.getServiceId())
+                                                                .address(instance.getHost())
+                                                                .meta(Collections.emptyMap())
+                                                                .build();
+                    ServiceHealth.Service service = ServiceHealth.Service.builder()
+                                                                         .id(instance.getServiceId())
+                                                                         .name(instance.getServiceId())
+                                                                         .tags(Arrays.asList(NO_SERVICE_TAGS))
+                                                                         .address(instance.getHost())
+                                                                         .meta(Collections.emptyMap())
+                                                                         .port(instance.getPort())
+                                                                         .build();
+                    ServiceHealth.Check check = ServiceHealth.Check.builder()
+                                                                   .node(node.getName())
+                                                                   .checkID("service:" + service.getId())
+                                                                   .name("Service '" + service.getName() + "' check")
+                                                                   .status("passing")
+                                                                   .build();
+                    ServiceHealth ipObj = ServiceHealth.builder()
+                                                       .node(node)
+                                                       .service(service)
+                                                       .checks(Collections.singletonList(check))
+                                                       .build();
+
+                    list.add(ipObj);
+                }
+                return list;
+            }
+        });
+    }
+
+    private <T> Single<ChangeItem<T>> returnDeferred(long waitMillis, Long index, Supplier<T> fn) {
+        return Single.just(new ChangeItem<>(fn.get(), new Date().getTime()));
+    }
 }
